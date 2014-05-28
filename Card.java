b11a88194ffe4958c462eea49f01807f8169b27b
Sungwoo Park <iyw03211@gmail.com>
2014-05-28 17:44:02 -0400

    Coded Card.java

---
+++ Card.java
@@ -0,0 +1,30 @@
+public class Card{
+    private String name; //Name of a word
+    private String def; //Defintion
+    private int numWrong; //The number of times a user got this word incorrect
+
+    //Constructor takes name
+    public Card(String n){
+        String name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDef(){
+        return def;
+    }
+
+    public void setDef(String definition){
+        def = defintion;
+    }
+
+    public int getNumWrong(){
+        return numWrong;
+    }
+
+    public void setNumWrong(int n){
+        numWrong = n;
+    }
+}
