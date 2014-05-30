33092dfea583e2f1afe9d8f4f7552ad1d1ef26ae
PratishG <gheewala.pratish@gmail.com>
2014-05-30 00:45:35 -0400

    Update Test.java
    I wasn't sure exactly how the user interface would work, so I stopped after initializing the RQueue.  Where would the user's answer go, and what would it look like?

---
+++ Test.java
@@ -0,0 +1,29 @@
+public class Test{
+	private RQueue<Card> Testable;
+	public Test(ArrayList<Card> Words){
+	//Random Queue with words from ArrayList.
+		Testable = new RQueue();
+		for(Card x: Words){
+		
+			Testable.enqueue(x);
+			if(x.getNumWrong > 0){
+				int numElem = x.getNumWrong;
+				while (numElem > 0){
+					Testable.enqueue(x);
+					numElem --;
+					}	
+				}
+			}
+		}
+		//this selects the word from the random queue
+	private Card picker(){
+		Card returner = Testable.peekFront();
+		return returner;
+		}
+		
+		//actual tester
+	//If user gets it right, remove word from the queue, else add the word.  If no more words left inform user
+	public 	void Tester(){
+
+	}
+	//unsure how exactly the program would work, how would it check if it user's response is right or wrong	
