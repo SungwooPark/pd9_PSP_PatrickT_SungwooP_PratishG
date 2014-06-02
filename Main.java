3fee50d326e0b0679f4092f70a68efa7b36cdcf5
Patrick Tsai <patosai@live.com>
2014-06-02 14:24:55 -0400

    Cleaned up main class

--- Main.java
+++ Main.java
@@ -1,28 +1,31 @@
 import java.util.*;
 
-public class Main {
+public class Main extends JPanel{
 
 	private ArrayList<Card> deck;
 	private boolean cont; //while this is true, program continues
 
 	public Main() {
-            deck = new ArrayList<Card>();
-            cont = true;
+        deck = new ArrayList<Card>();
 	}
 
-        public static void main(String[] args){
-            Main m = new Main();
+	public final initializeGUI() {
 
-            //Calls main screen method
-            while (m.cont()){
-                m.mainScreen();
-            }
+	}
 
+   	public static void main(String[] args){
+   		JFrame frame = new JFrame();
+        Main m = new Main();
+
+        //Calls main screen method
+        while (m.cont()){
+            m.mainScreen();
         }
+    }
 	
 	public void add(String term, String definition) {
-		Card newCard = new Card(term, definition);
-                deck.add(newCard); //Adding new word to the deck
+			Card newCard = new Card(term, definition);
+            deck.add(newCard); //Adding new word to the deck
         }
 	
 	public void remove(String term) {
@@ -33,44 +36,39 @@ public class Main {
 			}
 		}
 	}
-    
-        //Decidesss whether program continues or not
-        public boolean cont(){
-            return cont;
-        }
 
-        public void mainScreen(){
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Choose what you want to do");
-            System.out.print("1. Input new word 2. View inputted words 3. Remove words 4. Test 5. End Program :");
-            int command  = scan.nextInt();
-            //adding new word
-            if (command == 1){
-                System.out.print("Input your word: ");
-                String word = scan.next();
-                System.out.print("Input definition: ");
-                String def = scan.next();
-                add(word, def);
-            }
-            //View inputted words
-            else if(command == 2){
-                for (Card i: deck){
-                    System.out.println(i.getName());
-                }
-            }
-            //Removing words
-            else if (command == 3){
-                System.out.print("Which word do you want to remove? ");
-                String word = scan.next();
-                remove(word);
-            }
-            //Test
-            else if (command == 4){
-                //Call testing function/class
-            }
-            else if(command == 5){
-                cont = false;
-                System.out.println("Ending program");
+    public void mainScreen(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choose what you want to do");
+        System.out.print("1. Input new word 2. View inputted words 3. Remove words 4. Test 5. End Program :");
+        int command  = scan.nextInt();
+        //adding new word
+        if (command == 1){
+            System.out.print("Input your word: ");
+            String word = scan.next();
+            System.out.print("Input definition: ");
+            String def = scan.next();
+            add(word, def);
+        }
+        //View inputted words
+        else if(command == 2){
+            for (Card i: deck){
+                System.out.println(i.getName());
             }
         }
+        //Removing words
+        else if (command == 3){
+            System.out.print("Which word do you want to remove? ");
+            String word = scan.next();
+            remove(word);
+        }
+        //Test
+        else if (command == 4){
+            //Call testing function/class
+        }
+        else if(command == 5){
+            System.out.println("Ending program");
+            System.exit(0);
+        }
+    }
 }
