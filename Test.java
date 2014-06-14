a9f6d6665bb11a95944e9e19b14ce35bfd0b2da7
Sungwoo Park <iyw03211@gmail.com>
2014-06-14 16:01:06 -0400

    Checking whether test class works. Created main method to run this class only without main class

--- Test.java
+++ Test.java
@@ -2,23 +2,46 @@ import java.math.*;
 import java.util.*;
 
 public class Test{
-	public Card rightWord;
+	private Card rightWord;
 	//private int choice;
 	private RQueue<Card> Testable;
 	private int rightIndex; //Index of position for right index
-       
+        private ArrayList<Card> choiceWords; //temporary storage of words that are in the choices given to the user.
+        //This temporary storage is necessary to make sure that we do not have duplicate answer choice for the reader.
+
         //Main function created to check whether this class works. Will be deleted
         
         public static void main(String[] args){
             Card newCard = new Card("hello","world");
             Card newCard2 = new Card("apple","fruit");
+            Card newCard3 = new Card("banana","another fruit");
+            Card newCard4 = new Card("kiwi","delicious fruit");
+            Card newCard5 = new Card("orange","good fruit");
+
             ArrayList<Card> testDeck = new ArrayList<Card>();
             testDeck.add(newCard);
             testDeck.add(newCard2);
+            testDeck.add(newCard3);
+            testDeck.add(newCard4);
+            testDeck.add(newCard5);
 
             Test testClass = new Test(testDeck);
+            System.out.println(testClass.getRightWord().getName());
+            String[] userChoices = testClass.choicesPopulate();
+            for (String s:userChoices){
+                System.out.println(s);
+            }
         }
-        
+    
+        //Temporary test functions
+        public void checkTestable(){
+            while (!Testable.isEmpty()){
+                System.out.println(Testable.dequeue().getName());
+            }
+        }
+        public Card getRightWord(){
+            return rightWord;
+        }
         
         public Test(ArrayList<Card> Words){
 
@@ -49,22 +72,18 @@ public class Test{
 		
 		
 	//populate an array with wrong, and right words.	
-	private String[] choicesPopulate(){
+	public String[] choicesPopulate(){
 		Random  r = new Random();
 		//Changed var name Choices to choices. We usually make var name lowercase.
                 String[] choices = new String[4];
 		rightWord = picker();
-		/* This while loop is unnecessary.
-                 * for (int i = 0; i < 1, i ++){
-			Choices[r.nextInt(4)] = rightWord.getDef();
-			}*/
+                choiceWords.add(rightWord);
                 choices[r.nextInt(4)] = rightWord.getDef();
 		for (int i = 0; i < 3; i ++){
 			if(choices[i] == null){
-
-                                //Don't we have to use dequeue here too? Why peekfront?
-				//choices[i]=Testable.peekFront().getDef();
-			    choices[i] = Testable.dequeue().getDef();
+                            Card choiceToAdd = Testable.dequeue();
+                            choiceWords.add(choiceToAdd);
+			    choices[i] = choiceToAdd.getDef();
 		    }
                 }
                     //Return arraylist with the definition choice for user
